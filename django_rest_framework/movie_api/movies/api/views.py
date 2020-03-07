ce575d63100761c3767b9e55469cc5e6572aa69d
qraft technologies Q <koko8624@gmail.com>
2020-03-07 17:27:16 +0900

    feat: 함수형뷰와 클래스형 뷰 추가

--- django_rest_framework/movie_api/movies/api/views.py
+++ django_rest_framework/movie_api/movies/api/views.py
@@ -1,29 +1,72 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from rest_framework import viewsets
 from rest_framework.generics import (
-    ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
-    )
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+    CreateAPIView,
+)
 
 from movies.models import Movie
 from .serializers import *
 
+"""
+클래스형 뷰 (CBV, Class Based View)
 
 class MovieListAPIView(ListAPIView):
     queryset = Movie.objects.all()
-    serializer_class = MovieListSerializer
+    serializer_class = MovieSerializer
 
 
 class MovieDetailAPIView(RetrieveAPIView):
     lookup_field = "id"
     queryset = Movie.objects.all()
-    serializer_class = MovieDetailSerializer
-    
+    serializer_class = MovieSerializer
+
+
 class MovieUpdateAPIView(UpdateAPIView):
     lookup_field = "id"
     queryset = Movie.objects.all()
-    serializer_class = MovieListSerializer
+    serializer_class = MovieSerializer
+
 
 class MovieDestroyAPIView(DestroyAPIView):
     lookup_field = "id"
     queryset = Movie.objects.all()
-    serializer_class = MovieListSerializer
+    serializer_class = MovieSerializer
 
+
+class MovieCreateAPIView(CreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+"""
+
+# 함수형 뷰 (FBV, Function Based View)
+def index(request):
+    """ List all movies or create a new movie """
+
+    if request.method == "GET":
+        queryset = Movie.objects.all()
+        serializer = MovieSerializer(queryset, many=True)
+
+        return JsonResponse(
+            serializer.data, safe=False, json_dumps_params={"ensure_ascii": False}
+        )
+
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = MovieSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(
+                serializer.data, status=201, json_dumps_params={"ensure_ascii": False}
+            )
+
+        return JsonResponse(
+            serializer.errors, status=400, json_dumps_params={"ensure_ascii": False}
+        )
