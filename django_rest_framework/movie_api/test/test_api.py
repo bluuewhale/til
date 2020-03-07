6170a3ab6ae5931a04bef653e9a2e6f2c2b5a2f8
qraft technologies Q <koko8624@gmail.com>
2020-03-07 17:28:46 +0900

    Movie API 유닛테스트 코드 업로드
    현재는 테스트로 index(listView)에 대한 코드만 작성
    
    TODO
    datail, update, delete, create 코드 추가하기

---
+++ django_rest_framework/movie_api/test/test_api.py
@@ -0,0 +1,30 @@
+import requests
+import unittest
+
+
+class MovieAPITest(unittest.TestCase):
+    """ Moive API Test """
+
+    def test_index(self):
+        url = "http://localhost:8000/movies/api"
+
+        response = requests.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertIsInstance(data, list)
+
+    def test_detail(self):
+        raise NotImplementedError()
+
+    def test_delete(self):
+        raise NotImplementedError()
+
+    def test_create(self):
+        raise NotImplementedError()
+
+
+if __name__ == "__main__":
+
+    unittest.main()
