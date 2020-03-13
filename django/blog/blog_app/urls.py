0975e636e996961421ce46a506d7859f746c46a0
qraft technologies Q <koko8624@gmail.com>
2020-03-13 19:45:56 +0900

    feat: Django blog 튜토리얼 완료

---
+++ django/blog/blog_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, re_path
+from blog_app import views
+
+urlpatterns = [
+    re_path(r"^$", views.post_list),
+    re_path(r"(?P<pk>\d+)", views.post_detail),
+    re_path(r"^create", views.post_create),
+]
