b334f7c75f1c246f27b8eeece2ca12f8bf8b5b2c
qraft technologies Q <koko8624@gmail.com>
2020-03-17 01:14:38 +0900

    fix: reverse error fix

--- django/poll/polls/urls.py
+++ django/poll/polls/urls.py
@@ -6,6 +6,6 @@ app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:id>/", views.detail, name="detail"),
-    path("<int:id>/result", views.result, name="result"),
-    path("<int:id>/vote", views.vote, name="vote"),
+    path("<int:id>/results/", views.results, name="results"),
+    path("<int:id>/vote/", views.vote, name="vote"),
 ]
