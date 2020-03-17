720529888f4d7fdbe46e239f0787715a629916e1
qraft technologies Q <koko8624@gmail.com>
2020-03-17 22:25:31 +0900

    feat: admin 페이지 수정

--- django/poll/polls/admin.py
+++ django/poll/polls/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "text"]
+    list_display = ["text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["text"]
+    fieldsets = [
+        (None, {"fields": ["text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
