224966bee6932093a20c5f034d04d5b670117029
qraft technologies Q <koko8624@gmail.com>
2020-03-17 17:31:05 +0900

    test: tr_request app API  테스트 코드 작성

--- django/kiwoom_rest_api/tr_request/urls.py
+++ django/kiwoom_rest_api/tr_request/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path, include
 from tr_request import views
 
+app_name='tr_request'
 
 urlpatterns = [
-    re_path('', views.tr_request)
+    re_path('', views.tr_request, name='request')
 ]
     
