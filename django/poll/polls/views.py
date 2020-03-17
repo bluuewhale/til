9e38882b174673f11085ba8f222bd4b096a26ad1
qraft technologies Q <koko8624@gmail.com>
2020-03-17 13:54:04 +0900

    feat: generic view로 수 수정

--- django/poll/polls/views.py
+++ django/poll/polls/views.py
@@ -1,28 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+
 from .models import Question, Choice
 
 response = "Question {} : {}"
 
 
-def index(request):
-    questions = Question.objects.order_by("-pub_date")
-    return render(request, "polls/index.html", {"questions": questions})
+class IndexView(generic.ListView):
+    template_name = "polls/question_list.html"
+    context_object_name = "questions"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")
 
 
-def detail(request, id):
-    question = get_object_or_404(Question, pk=id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/question_detail.html"
 
 
-def results(request, id):
-    question = get_object_or_404(Question, pk=id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
-def vote(request, id):
-    question = get_object_or_404(Question, pk=id)
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
@@ -34,5 +39,5 @@ def vote(request, id):
 
     selected_choice.votes += 1
     selected_choice.save()
-    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    return HttpResponseRedirect(reverse("polls:results", args=(question.pk,)))
 
