d4f53e1aa06c22f0fcbced491222c571f11631d1
qraft technologies Q <koko8624@gmail.com>
2020-03-17 13:54:04 +0900

    feat: generic view로 수 수정

--- django/poll/polls/urls.py
+++ django/poll/polls/urls.py
@@ -4,8 +4,8 @@ from polls import views
 app_name = "polls"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:id>/", views.detail, name="detail"),
-    path("<int:id>/results/", views.results, name="results"),
-    path("<int:id>/vote/", views.vote, name="vote"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultView.as_view(), name="results"),
+    path("<int:pk>/vote/", views.vote, name="vote"),
 ]
