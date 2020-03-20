48d93199ac6875236262bdec8d040bcca4ff6994
qraft technologies Q <koko8624@gmail.com>
2020-03-20 21:26:04 +0900

    feat: remove --cached

--- django/django_official_documents/db_models/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DbModelsConfig(AppConfig):
-    name = 'db_models'
--- django/movie_api/movies/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from movies.models import Movie
-
-
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie  # 모델 설정
-        fields = ("pk", "title", "genre", "year")  # 필드 설정
