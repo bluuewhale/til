be02756b0a4b3a8b95d9a0a90001c11b70b0eb40
qraft technologies Q <koko8624@gmail.com>
2020-03-20 21:26:04 +0900

    feat: remove --cached

--- django/blog/blog_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path
-from blog_app import views
-
-urlpatterns = [
-    re_path("^$", views.post_list),
-    re_path(r"/(?P<pk>\d+)", views.post_detail),
-    re_path(r"^/create", views.post_create),
-]
