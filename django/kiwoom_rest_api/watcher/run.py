89599de95c38771c1abf6887139724a9850633ed
Donghyung Ko <koko8624@gmail.com>
2020-03-23 11:59:07 +0900

    fix: Folder name change

--- django/kiwoom_rest_api/watcher/run.py
+++ django/kiwoom_rest_api/watcher/run.py
@@ -5,23 +5,25 @@ from PyQt5.QtWidgets import QApplication
 from kiwoom_api.api import Kiwoom, DataFeeder, Executor
 from watchers import TrWatcher, OrderWatcher
 
+
 def main():
     app = QApplication(sys.argv)
 
-    kiwoom = Kiwoom()
+    kiwoom = Kiwoom.instance()
     kiwoom.commConnect()
     feeder = DataFeeder(kiwoom)
     executer = Executor(kiwoom)
 
     tr_watcher = TrWatcher(kiwoom, feeder)
     order_watcher = OrderWatcher(kiwoom, executer)
-    
+
     loop = asyncio.get_event_loop()
     coroutineList = [
-        tr_watcher.run(main_func_name='request'),
-        order_watcher.run(main_func_name='sendOrder'),
+        tr_watcher.run(main_func_name="request"),
+        order_watcher.run(main_func_name="sendOrder"),
     ]
     loop.run_until_complete(asyncio.gather(*coroutineList))
-    
+
+
 if __name__ == "__main__":
     main()
