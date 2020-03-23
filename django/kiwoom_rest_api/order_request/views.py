6a3ddeb8ca55cf12cf7dc587c3dc4d7192500439
Donghyung Ko <koko8624@gmail.com>
2020-03-23 11:59:07 +0900

    fix: Folder name change

--- django/kiwoom_rest_api/order_request/views.py
+++ django/kiwoom_rest_api/order_request/views.py
@@ -3,20 +3,22 @@ from datetime import datetime as dt
 import os
 import sys
 import time
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from django.conf import settings 
+from django.conf import settings
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
 from manager import OrderManager
 
-@api_view(['POST'])
-@permission_classes((IsAuthenticated, ))
+
+@api_view(["POST"])
+@permission_classes((IsAuthenticated,))
 def order_request(request):
-    req_dir = os.path.join(settings.BASE_DIR, 'watcher/tmp/order/requests')
-    res_dir = os.path.join(settings.BASE_DIR, 'watcher/tmp/order/responses')
-    
+    req_dir = os.path.join(settings.BASE_DIR, "watcher/cache/order/requests")
+    res_dir = os.path.join(settings.BASE_DIR, "watcher/cache/order/responses")
+
     manager = OrderManager(req_dir, res_dir)
-    data =  manager.run(request)
+    data = manager.run(request)
     return data
--- django/kiwoom_rest_api/tr_request/views.py
+++ django/kiwoom_rest_api/tr_request/views.py
@@ -3,19 +3,21 @@ from datetime import datetime as dt
 import os
 import sys
 import time
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from django.conf import settings 
+from django.conf import settings
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from manager import TrManager
 
-@api_view(['GET'])
-@permission_classes((IsAuthenticated, ))
+
+@api_view(["GET"])
+@permission_classes((IsAuthenticated,))
 def tr_request(request):
-    req_dir = os.path.join(settings.BASE_DIR, 'watcher/tmp/tr/requests')
-    res_dir = os.path.join(settings.BASE_DIR, 'watcher/tmp/tr/responses')
-    
+    req_dir = os.path.join(settings.BASE_DIR, "watcher/cache/tr/requests")
+    res_dir = os.path.join(settings.BASE_DIR, "watcher/cache/tr/responses")
+
     manager = TrManager(req_dir, res_dir)
-    data =  manager.run(request)
+    data = manager.run(request)
     return data
