ca4931f3a6da48d733757406585d2c7bc94404b7
DonghyungKo <koko8624@gmail.com>
2020-05-31 17:54:58 +0900

    feat: ch05 구조체(structure) updated

---
+++ ch05_structure/target/debug/deps/ch05_structure-39e3954708e3457a.d
@@ -0,0 +1,5 @@
+/Users/donghyungko/Documents/git-project/rust/the_rust_programming_language/ch05_structure/target/debug/deps/ch05_structure-39e3954708e3457a: src/main.rs
+
+/Users/donghyungko/Documents/git-project/rust/the_rust_programming_language/ch05_structure/target/debug/deps/ch05_structure-39e3954708e3457a.d: src/main.rs
+
+src/main.rs:
