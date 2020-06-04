a859f2f2a755f6c89dcbb3f0623ba9a066ca28f0
DonghyungKo <koko8624@gmail.com>
2020-06-04 22:40:53 +0900

    feat: ch14 iterator and closure update

---
+++ ch13_iterator_and_closure/closures/src/main.rs
@@ -0,0 +1,9 @@
+use ::closures;
+
+fn main() {
+    let intensity = 10;
+    let salt = 7;
+    //closures::generate_workout_enhanced(intensity, salt);
+    closures::test_capture();
+    closures::test_fnonce();
+}
---
+++ ch13_iterator_and_closure/target/debug/deps/ch13_iterator_and_closure-df7d676f617f2523.d
@@ -0,0 +1,5 @@
+/Users/donghyungko/Documents/git-project/rust/the_rust_programming_language/ch13_iterator_and_closure/target/debug/deps/ch13_iterator_and_closure-df7d676f617f2523.rmeta: src/main.rs
+
+/Users/donghyungko/Documents/git-project/rust/the_rust_programming_language/ch13_iterator_and_closure/target/debug/deps/ch13_iterator_and_closure-df7d676f617f2523.d: src/main.rs
+
+src/main.rs:
