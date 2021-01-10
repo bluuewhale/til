113ae21716269fd787e7f6ec054e3e24b01bdbe9
DonghyungKo <koko8624@gmail.com>
2021-01-10 22:19:40 +0900

    F: terraform tutorial update

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,8 @@
+web_server_location      = "koreacentral"
+web_server_rg            = "web-rg"
+resource_prefix          = "web-server"
+web_server_name          = "web-01"
+environment              = "development"
+web_server_count         = 2
+terraform_script_version = "1.0.0"
+admin_password           = "passw0rd1234!"
---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "web_server_location" {
+  type = string
+}
+
+variable "web_server_rg" {
+  type = string
+}
+
+variable "resource_prefix" {
+  type = string
+}
+
+variable "web_server_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "web_server_count" {
+  type = number
+}
+
+variable "terraform_script_version" {
+  type = string
+}
+
+variable "admin_password" {
+  type = string
+}
