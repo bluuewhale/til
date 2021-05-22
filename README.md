22bbe00e3b5c09e2eb76f11c2cbe68723255a643
BlueWhaleKo <koko8624@gmail.com>
2021-05-23 00:37:15 +0900

    D: a new post added to README.md

--- README.md
+++ README.md
@@ -107,9 +107,10 @@ This repository is where I write code implementations of Effective Jave 3rd edit
 ## Spring101
 ### Contents
 
+- [`ToyProject`: Hello Spring!](https://github.com/DonghyungKo/til/tree/master/java/spring101/hello-spring)
 - [`DI: Dependency Injection`](https://velog.io/@koo8624/Spring101-%EC%9D%98%EC%A1%B4%EC%84%B1-%EC%A3%BC%EC%9E%85Dependency-Injection)
 - [`AOP: Aspect Oriented Programming`](https://velog.io/@koo8624/Spring101-%EA%B4%80%EC%A0%90-%EC%A7%80%ED%96%A5-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8DAOP)
-- [`ToyProject`: Hello Spring!](https://github.com/DonghyungKo/til/tree/master/java/spring101/hello-spring)
+- [`Test: Test With @SpringBootTest`](https://velog.io/@koo8624/Spring-%EC%8A%A4%ED%94%84%EB%A7%81-%EB%B6%80%ED%8A%B8%EC%97%90%EC%84%9C-%ED%86%B5%ED%95%A9-%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%8B%A4%ED%96%89%ED%95%98%EA%B8%B0)
 
 
 &nbsp;
--- java/spring101/hello-spring/src/test/java/com/donghyungko/hellospring/service/MemberServiceIntTest.java
+++ java/spring101/hello-spring/src/test/java/com/donghyungko/hellospring/service/MemberServiceIntTest.java
@@ -1,29 +1,24 @@
 package com.donghyungko.hellospring.service;
 
 import com.donghyungko.hellospring.domain.Member;
+import com.donghyungko.hellospring.repository.MemberRepository;
 import com.donghyungko.hellospring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class MemberServiceTest {
 
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
-
-    @BeforeEach
-    public void beforeEach() {
-        this.memberRepository = new MemoryMemberRepository();
-        this.memberService = new MemberService(this.memberRepository);
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+@SpringBootTest
+@Transactional
+public class MemberServiceIntTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     void 회원가입() {
@@ -55,7 +50,6 @@ class MemberServiceTest {
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
         //then
-
     }
 
 
@@ -71,4 +65,5 @@ class MemberServiceTest {
     @Test
     void 회원조회() {
     }
+
 }
