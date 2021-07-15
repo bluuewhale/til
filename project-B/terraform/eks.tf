5defad8e5ba65be25db57e12d0ee9832a1aa5a0c
BlueWhaleKo <koko8624@gmail.com>
2021-07-15 13:53:41 +0900

    fix: fix enable private access in eks cluster

--- project-B/terraform/eks.tf
+++ project-B/terraform/eks.tf
@@ -5,8 +5,10 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.cluster.arn
 
   vpc_config {
-    public_access_cidrs = local.cidr_blocks_sucho
-    subnet_ids          = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+    public_access_cidrs     = local.cidr_blocks_sucho
+    subnet_ids              = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+    endpoint_private_access = local.endpoint_private_access
+    endpoint_public_access  = local.endpoint_public_access
   }
 
   kubernetes_network_config {
--- project-B/terraform/local.tf
+++ project-B/terraform/local.tf
@@ -18,6 +18,8 @@ locals {
   cluster_version              = var.cluster_version != "" ? var.cluster_version : "1.17"                                    // Version of the EKS cluster
   cluster_suffix               = var.cluster_suffix != "" ? var.cluster_suffix : local.cluster_name                          // EKS cluster resource suffix
   kubernetes_service_ipv4_cidr = var.kubernetes_service_ipv4_cidr != "" ? var.kubernetes_service_ipv4_cidr : "172.20.0.0/16" // CIDR block for Kubernetes Services
+  endpoint_private_access      = true
+  endpoint_public_access       = true
 
   // [Node Group]
   webapp_ami_name_filter = var.webapp_ami_name_filter != "" ? var.webapp_ami_name_filter : "amazon-eks-node-${local.cluster_version}-v*" // AMI Name filter
