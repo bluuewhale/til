5ff4f749d47497f3c62a1dd933900d79b5b686aa
BlueWhaleKo <koko8624@gmail.com>
2021-07-15 21:16:35 +0900

    feature: add feature to enable changing kubernetes context to apply_kubeconfig.sh

--- project-B/terraform/apply_kubeconfig.sh
+++ project-B/terraform/apply_kubeconfig.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-set -xe
 
-terraform output -raw kubeconfig > ~/.kube/eks-webapp.config
-export KUBECONFIG=~/.kube/eks-webapp.config
+# [Description]
+# merge new kubeconfig from EKS cluster created by Terraform to current kubeconfig.
+# Then, move to new context 'aws'
+#
+# [Maintainer]
+# Donghyung Ko <koko8624@pubg.com>
+
+set -e
+
+if [[ -z $KUBECONFIG ]]; then
+    OLD_KUBECONFIG="$HOME/.kube/config"
+fi
+
+# create new config file from Terraform output
+NEW_KUBECONFIG="$HOME/.kube/eks-webapp.config"
+terraform output -raw kubeconfig > $NEW_KUBECONFIG
+chmod 600 ~/.kube/eks-webapp.config
+
+# merge config files
+KUBECONFIG=$OLD_KUBECONFIG:$NEW_CONFIG_PATH 
+kubectl config view --merge --flatten >  ~/.kube/merged.config 
+KUBECONFIG="$HOME/.kube/merged.config"
+echo "Successfully merged KUBECONFIG to ${KUBECONFIG}"
+
+# change context
+kubectx aws
+echo "You are temporarily able to access EKS cluster by using 'kubectl' command with this terminal session only"
